4188183b3711e4430ab3f77eebbdf3f8c3e44dab
Ben <ben.crommen@gmail.com>
2020-08-27 02:11:20 +0200

    404 + PDF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { ContactPageComponent } from './components/contact-page/contact-page.component';
-import { BlogPageComponent } from './components/blog-page/blog-page.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { ContactPageComponent } from "./components/contact-page/contact-page.component";
+import { BlogPageComponent } from "./components/blog-page/blog-page.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
@@ -18,13 +18,10 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     HomePageComponent,
     ContactPageComponent,
     BlogPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
